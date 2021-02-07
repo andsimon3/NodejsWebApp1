9cabafe331d778f64582f897f36329cabd1f1d40
andsimon3 <simon.03@mail.ru>
2021-02-07 14:43:02 +0300

    Чатик вернулся

--- clientSide/room/chat.tsx
+++ clientSide/room/chat.tsx
@@ -1,36 +1,19 @@
 declare var require: any
 import * as React from 'react';
+import { observer } from 'mobx-react-lite';
+import { room } from './../room';
 
-export class Chat extends React.Component<{roomId}, { messages }> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-		}
-	}
-    render() {
-        function send() { }/*
-            let answer = {
-                type: 'message',
-                data: (document.getElementById('test') as HTMLInputElement).value,
-            }
-            //socket.send(JSON.stringify(answer));
-        }
-        socket.onmessage = (event) => {
-            let message = JSON.parse(event.data);
-            switch (message.type) {
-                case 'newMessage':
-                    this.setState({ messages: [...this.state.messages, message.data] });
-                    break;
-            };
-        }*/
-        return (
-            <div>
-                <p>{this.props.roomId}</p><br/>
-                <div>{/*this.state.messages.join('\n ')*/}</div>
-                <input type='text' name='room_code' id='test' />
-                <button onClick={(e) => send()}>Go</button>
-            </div>
-        )
+export const Chat = observer(() => {
+
+    function send() {
+        room.sendChat((document.getElementById('test') as HTMLInputElement).value)
     }
-}
+    return (
+        <div>
+            <p>{room.roomId} <br /> {room.members.join(', ')} <br /> {room.chat.map((text) => { return <p key={text}>{text}</p> })}</p><br />
+            <div>{/*this.state.messages.join('\n ')*/}</div>
+            <input type='text' name='room_code' id='test' />
+            <button onClick={(e) => send()}>Go</button>
+        </div>
+    )
+});
